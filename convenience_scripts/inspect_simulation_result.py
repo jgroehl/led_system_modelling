b9d61804adf01d847eac8afe06be378a9cea6a72
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-08 13:02:40 +0200

    Improve absorption maps
    Plot the absorption in separate figures. Add colorbar, labels and a
    title. Everything is still in arbitrary units.

--- convenience_scripts/inspect_simulation_result.py
+++ convenience_scripts/inspect_simulation_result.py
@@ -11,14 +11,22 @@ HDF5_PATH = os.path.join(base_dir, path_manager.get_hdf5_file_save_path(), "opti
 p0 = sp.load_data_field(HDF5_PATH, sp.Tags.DATA_FIELD_INITIAL_PRESSURE, 800)
 # p0 = np.log(p0)
 x, y, z = p0.shape
-print(x, y, z)
+print("Simulation dimensions:\n", x, y, z)
 spacing = (5/x) * 10
 
-plt.subplot(1, 2, 1)
+plt.figure()
 plt.imshow(p0[:, int(y/2), :].T, extent=[0, x*spacing, z*spacing, 0])
+plt.xlabel("x-position [a.u.]")
+plt.ylabel("z-position [a.u.]")
+plt.colorbar(label="Absorption [a.u.]")
+plt.title("Absorption map")
 
-plt.subplot(1, 2, 2)
+plt.figure()
 plt.imshow(p0[int(x/2), :, :].T, extent=[0, y*spacing, z*spacing, 0])
+plt.xlabel("y-position [a.u.]")
+plt.ylabel("z-position [a.u.]")
+plt.colorbar(label="Absorption [a.u.]")
+plt.title("Absorption map (side view)")
 
 plt.show()
 
