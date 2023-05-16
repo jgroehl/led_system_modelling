957bd7cf861ae605f965d89bc509b71eaf7e4dfb
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-16 13:37:41 +0200

    Document run_simulations
    Add documentation to the run_simulations.py script, and move the
    contents into a function. Now, the function could in theory also be used
    indirectly (i.e. the program is not directly run by the Python
    interpreter).

--- run_simulations.py
+++ run_simulations.py
@@ -7,52 +7,85 @@ from utils.noise_models import add_noise
 DATA_FILE = "phantoms/veins.npz"
 NOISE_FILE = "resources/NoiseMeasurement.mat"
 
-# Construct absolute paths from the relative paths defined above
-abs_data_file = os.path.abspath(DATA_FILE)
-abs_noise_file = os.path.abspath(NOISE_FILE)
 
-path = simulate(abs_data_file, "baseline")
-add_noise(path, abs_noise_file)
+def run_simulations(data_file: str, noise_file: str) -> None:
+    """
+    Run all photoacoustic simulation pipelines on a phantom with a given noise file.
+    This will run acoustic simulations, as well as a photoacoustic simulations.
+    Both will get simulated multiple times, with the following additional features:
+    - None (baseline)
+    - Acoustic attenuation
+    - Detector size
+    - Frequency response
+    - Acoustic attenuation + detector size
+    - Acoustic attenuation + detector size + frequency response
+    It will also apply noise based on a specified noise file over all simulations.
 
-path = simulate(abs_data_file, "attenuation", model_acoustic_attenuation=True)
-add_noise(path, abs_noise_file)
+    :param str data_file: The absolute path to the phantom 'ground truth' file (.npz file)
+    :param str noise_file: The absolute path to the noise measurement (.mat file)
+    """
 
-path = simulate(abs_data_file, "size", model_detector_size=True)
-add_noise(path, abs_noise_file)
+    # Acoustic simulation
+    path = simulate(data_file, "baseline")
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "frequencyresponse", model_frequency_response=True)
-add_noise(path, abs_noise_file)
+    # Acoustic simulation + acoustic attenuation
+    path = simulate(data_file, "attenuation", model_acoustic_attenuation=True)
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "attenuation_size", model_acoustic_attenuation=True,
-                model_detector_size=True)
-add_noise(path, abs_noise_file)
+    # Acoustic simulation + detector size
+    path = simulate(data_file, "size", model_detector_size=True)
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "attenuation_size_frequencyresponse", model_acoustic_attenuation=True,
-                model_detector_size=True, model_frequency_response=True)
-add_noise(path, abs_noise_file)
+    # Acoustic simulation + frequency response
+    path = simulate(data_file, "frequencyresponse", model_frequency_response=True)
+    add_noise(path, noise_file)
 
-# We only have to run the optical forward model once per phantom digital twin.
-optical_path = simulate(abs_data_file, "optical", optical_model=True)
-add_noise(optical_path, abs_noise_file)
-optical_path_simpa = optical_path.replace("_ipasc", "")
+    # Acoustic simulation + acoustic attenuation + detector size
+    path = simulate(data_file, "attenuation_size", model_acoustic_attenuation=True,
+                    model_detector_size=True)
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "optical_attenuation", optical_model=True, model_acoustic_attenuation=True,
-                load_initial_pressure_path=optical_path_simpa)
-add_noise(path, abs_noise_file)
+    # Acoustic simulation + acoustic attenuation + detector size + frequency response
+    path = simulate(data_file, "attenuation_size_frequencyresponse", model_acoustic_attenuation=True,
+                    model_detector_size=True, model_frequency_response=True)
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "optical_size", optical_model=True, model_detector_size=True,
-                load_initial_pressure_path=optical_path_simpa)
-add_noise(path, abs_noise_file)
+    # Run optical forward model
+    optical_path = simulate(data_file, "optical", optical_model=True)
+    add_noise(optical_path, noise_file)
+    optical_path_simpa = optical_path.replace("_ipasc", "")
 
-path = simulate(abs_data_file, "optical_frequencyresponse", optical_model=True, model_frequency_response=True,
-                load_initial_pressure_path=optical_path_simpa)
-add_noise(path, abs_noise_file)
+    # Optical & acoustic simulation + acoustic attenuation
+    path = simulate(data_file, "optical_attenuation", optical_model=True, model_acoustic_attenuation=True,
+                    load_initial_pressure_path=optical_path_simpa)
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "optical_attenuation_size", optical_model=True, model_acoustic_attenuation=True,
-                load_initial_pressure_path=optical_path_simpa, model_detector_size=True)
-add_noise(path, abs_noise_file)
+    # Optical & acoustic simulation + detector size
+    path = simulate(data_file, "optical_size", optical_model=True, model_detector_size=True,
+                    load_initial_pressure_path=optical_path_simpa)
+    add_noise(path, noise_file)
 
-path = simulate(abs_data_file, "optical_attenuation_size_frequencyresponse", optical_model=True,
-                model_acoustic_attenuation=True, model_frequency_response=True,
-                load_initial_pressure_path=optical_path_simpa, model_detector_size=True)
-add_noise(path, abs_noise_file)
+    # Optical & acoustic simulation + frequency response
+    path = simulate(data_file, "optical_frequencyresponse", optical_model=True, model_frequency_response=True,
+                    load_initial_pressure_path=optical_path_simpa)
+    add_noise(path, noise_file)
+
+    # Optical & acoustic simulation + acoustic attenuation + detector size
+    path = simulate(data_file, "optical_attenuation_size", optical_model=True, model_acoustic_attenuation=True,
+                    load_initial_pressure_path=optical_path_simpa, model_detector_size=True)
+    add_noise(path, noise_file)
+
+    # Optical & acoustic simulation + acoustic attenuation + detector size + frequency response
+    path = simulate(data_file, "optical_attenuation_size_frequencyresponse", optical_model=True,
+                    model_acoustic_attenuation=True, model_frequency_response=True,
+                    load_initial_pressure_path=optical_path_simpa, model_detector_size=True)
+    add_noise(path, noise_file)
+
+
+if __name__ == "__main__":
+    # Construct absolute paths from the relative paths defined above
+    abs_data_file = os.path.abspath(DATA_FILE)
+    abs_noise_file = os.path.abspath(NOISE_FILE)
+
+    run_simulations(abs_data_file, abs_noise_file)
