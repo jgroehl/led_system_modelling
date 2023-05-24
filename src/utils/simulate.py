0fcaf1ac8d8c15ea104111e162259b966baf391d
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-24 12:33:46 +0200

    Add comments to utils/simulate function

--- src/utils/simulate.py
+++ src/utils/simulate.py
@@ -34,20 +34,15 @@ def simulate(data_path, data_name,
     :rtype: str
     """
 
+    # Create SIMPA path manager and settings instance
     path_manager = sp.PathManager()
     settings = generate_base_settings(path_manager, volume_name=data_name)
 
-    # extract information on geometry and spacing for the purposes of volume creation and device definition
+    # Extract information on geometry and spacing of the phantom
     dim_x_mm = settings[Tags.DIM_VOLUME_X_MM]
     dim_y_mm = settings[Tags.DIM_VOLUME_Y_MM]
     dim_z_mm = settings[Tags.DIM_VOLUME_Z_MM]
     spacing = settings[Tags.SPACING_MM]
-
-
-    # ###################################################################################
-    # VOLUME CREATION
-    # Using the SIMPA volume creation module with the segmentation based volume creator
-    # ###################################################################################
 
     sizes = np.round(np.asarray([dim_x_mm, dim_y_mm, dim_z_mm]) / spacing).astype(int)
     sx, _, sz = sizes
@@ -56,7 +51,7 @@ def simulate(data_path, data_name,
     # Load the label mask from the ground truth data
     label_mask = np.load(data_path)["gt"].T
 
-    # scale the label mask based on the difference in spacing between the input and output
+    # Scale the label mask to fit the volume dimensions
     input_spacing = settings[Tags.SPACING_MM] / 2
     label_mask = np.round(zoom(label_mask, input_spacing/spacing, order=0)).astype(int)
     mx, mz = np.shape(label_mask)
@@ -74,24 +69,29 @@ def simulate(data_path, data_name,
         case (False, False):
             label_volume[dx:-dx, int(sizes[1]/2), dz:-dz] = label_mask
 
+    # Create volume (using SIMPA volume creation module, with segmentation based volume creator)
     settings.set_volume_creation_settings({
         Tags.INPUT_SEGMENTATION_VOLUME: label_volume,
         Tags.SEGMENTATION_CLASS_MAPPING: segmentation_class_mapping(model_acoustic_attenuation),
     })
 
+    # Extract acoustic settings and adapt
     acoustic_settings = settings.get_acoustic_settings()
     acoustic_settings["frequency_response"] = model_frequency_response
     acoustic_settings["detector_size"] = model_detector_size
 
     if optical_model:
-        # For this simulation: Use the created absorption map as the input initial pressure
         acoustic_settings[Tags.DATA_FIELD] = Tags.DATA_FIELD_INITIAL_PRESSURE
+        # If the simulation is run with an optical model, and an initial pressure distribution is given,
+        # use this pressure distribution
         if load_initial_pressure_path is not None:
             initial_pressure = sp.load_data_field(load_initial_pressure_path, sp.Tags.DATA_FIELD_INITIAL_PRESSURE, 800)
             pipeline = [
                 sp.SegmentationBasedVolumeCreationAdapter(settings),
                 IpascSimpaKWaveAdapter(settings, initial_pressure=initial_pressure)
             ]
+        # If the simulation is run with an optical model, but no initial pressure distribution is given,
+        # use the SIMPA MCX adapter
         else:
             pipeline = [
                 sp.SegmentationBasedVolumeCreationAdapter(settings),
@@ -105,12 +105,11 @@ def simulate(data_path, data_name,
             IpascSimpaKWaveAdapter(settings)
         ]
 
-    # Create the Cyberdyne LED-based system as the photoacoustic model
-    device = CyberdyneLEDArraySystem(device_position_mm=np.array([dim_x_mm/2,
-                                                                 dim_y_mm/2,
-                                                                 0]),
+    # Create the Cyberdyne LED-based system at the right position
+    device = CyberdyneLEDArraySystem(device_position_mm=np.array([dim_x_mm/2, dim_y_mm/2, 0]),
                                      field_of_view_extent_mm=np.asarray([-25, 25, 0, 0, 0, 40]))
 
+    # Run the simulation
     sp.simulate(simulation_pipeline=pipeline,
                 settings=settings,
                 digital_device_twin=device)
