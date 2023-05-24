f8323086b8b66c4f5764553b25d66497493da971
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-24 16:37:06 +0200

    WIP: normalize sinogram
    We want to normalize the simulation result in order to compare
    simulations with each other and to compare simulation results with
    experimental results. Needs to be tested on computer with PACFISH
    installed and results stored before pushing to main.

---
+++ src/utils/normalize_sinogram.py
@@ -0,0 +1,35 @@
+import pacfish as pf
+import simpa as sp
+import numpy as np
+import os.path
+
+# Define resulting mean and standard deviation
+MEAN = 50
+STDDEV = 10
+
+# Define path to sinogram to normalize when this file is run directly
+# by the Python interpreter
+INSPECT_PATH = "/tmp/results/sinogram.hdf5"
+
+def normalize_sinogram(data_path):
+    data = pf.load_data(data_path).binary_time_series_data
+
+    # Scale data to right standard deviation
+    stddev = np.std(data)
+    data *= STDDEV/stddev
+
+    # Shift data to right mean value
+    mean = np.mean(data)
+    data += MEAN - mean
+
+    return data
+
+
+if __name__ == "__main__":
+    normalized = normalize_sinogram(os.path.abspath(INSPECT_PATH))
+
+    # Inspect result
+    plt.figure()
+    plt.imshow(normalized, aspect=8)
+    plt.colorbar()
+    plt.show()
