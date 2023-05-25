da97d2cd0de63cbaf2843b505e5cedd10895c8dd
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-25 14:52:52 +0200

    Add possibility to normalize experimental sinogram
    Additionally, add documentation to normalization method.

--- src/utils/normalize_sinogram.py
+++ src/utils/normalize_sinogram.py
@@ -1,6 +1,7 @@
 import pacfish as pf
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.io import loadmat
 import os.path
 
 # Define resulting mean and standard deviation
@@ -9,19 +10,37 @@ STDDEV = 1
 
 # Define path to sinogram to normalize when this file is run directly
 # by the Python interpreter
-INSPECT_PATH = "/tmp/results/sinogram.hdf5"
+
+# simulation
+INSPECT_PATH = "../../results/optical_attenuation_size_ipasc.hdf5"
 
 
-def normalize_sinogram(data_path):
-    data = pf.load_data(data_path).binary_time_series_data
+def normalize_sinogram(data_path, experimental=False, mean=MEAN, stddev=STDDEV):
+    """
+    Normalize a sinogram according to given mean and standard deviation.
+
+    :param str data_path: Path to the data file. This can either be a simulation result (stored as ``.hdf5``) or
+                          an experimental result (stored under the ``sinogram`` tag in a ``.mat`` file)
+    :param bool experimental: Whether the given data file is an experimental result
+    :param float mean: The target mean for the normalized sinogram
+    :param float stddev: The target standard deviation for the normalized sinogram
+
+    :return: The normalized data
+    """
+
+    if experimental:
+        data_raw = loadmat(data_path)
+        data = data_raw["sinogram"].T
+    else:
+        data = pf.load_data(data_path).binary_time_series_data
 
     # Scale data to right standard deviation
-    stddev = np.std(data)
-    data *= STDDEV/stddev
+    old_stddev = np.std(data)
+    data *= stddev/old_stddev
 
     # Shift data to right mean value
-    mean = np.mean(data)
-    data += MEAN - mean
+    old_mean = np.mean(data)
+    data += mean - old_mean
 
     return data
 
