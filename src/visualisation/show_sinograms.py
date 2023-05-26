dc5f92d91ea4cfa65367fd5ac3586a9df54e67da
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-26 11:36:27 +0200

    Update show_sinograms
    Create a function, such that it is callable from a different entrypoint
    as well. Clean up code and add documentation.

--- src/visualisation/show_sinograms.py
+++ src/visualisation/show_sinograms.py
@@ -1,53 +1,59 @@
 import matplotlib.pyplot as plt
 import pacfish as pf
-import simpa as sp
-import os.path
 
-base_dir = os.path.join(os.path.curdir, "../../")
-path_manager = sp.PathManager("../path_config.env")
-BASE_PATH = os.path.abspath(os.path.join(base_dir, path_manager.get_hdf5_file_save_path()))
+NOISE = True
 
-plt.figure(figsize=(10, 12))
 
-plt.subplot(4, 2, 1)
-plt.title("baseline")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "baseline_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+def show_sinogram(plot_title, data_path, show=False):
+    """
+    Show a sinogram (pressure data over time for all detectors).
+    Plot can be shown immediately when using show=True, but this will cause the code to stop running and wait for the
+    figure to be closed before continuing.
 
-plt.subplot(4, 2, 3)
-plt.title("+ attenuation")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "attenuation_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+    :param str plot_title: The title to use for the sinogram plot
+    :param str data_path: The absolute path to the data file
+    :param bool show: Whether to immediately show the result
+    """
+    data = pf.load_data(data_path).binary_time_series_data
 
-plt.subplot(4, 2, 5)
-plt.title("+ directivity")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "attenuation_size_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+    plt.figure()
+    plt.title(plot_title)
+    plt.imshow(data, aspect=8)
+    plt.xlabel("Time step [ ]")
+    plt.ylabel("Detector index [ ]")
+    plt.colorbar(label="Pressure [a.u.]")
 
-plt.subplot(4, 2, 7)
-plt.title("+ frequency response")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "attenuation_size_frequencyresponse_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+    if show:
+        plt.show()
 
-plt.subplot(4, 2, 2)
-plt.title("optical")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "optical_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
 
-plt.subplot(4, 2, 4)
-plt.title("+ attenuation")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "optical_attenuation_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+if __name__ == "__main__":
+    import simpa as sp
+    import os.path
 
-plt.subplot(4, 2, 6)
-plt.title("+ directivity")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "optical_attenuation_size_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+    path_manager = sp.PathManager("../path_config.env")
+    base_dir = "../../"
 
-plt.subplot(4, 2, 8)
-plt.title("+ frequency response")
-plt.imshow(pf.load_data(os.path.join(BASE_PATH, "optical_attenuation_size_frequencyresponse_ipasc.hdf5")).binary_time_series_data[:, :1024], aspect=8)
-plt.colorbar()
+    simulations = {
+        "baseline": "baseline",
+        "acoustic attenuation": "attenuation",
+        "detector size": "size",
+        "frequency response": "frequencyresponse",
+        "acoustic attenuation + detector size": "attenuation_size",
+        "acoustic attenuation + detector size + frequency response": "attenuation_size_frequencyresponse",
+    }
 
-plt.tight_layout()
-plt.show()
+    for optical in [False, True]:
+        optical_str = "optical_" if optical else ""
+        for (title, filename) in simulations.items():
+            if optical:
+                title = "Optical + Acoustic:\n" + title
+            else:
+                title = "Acoustic:\n" + title
+            save_dir = os.path.abspath(os.path.join(base_dir, path_manager.get_hdf5_file_save_path()))
+            full_filename = optical_str + filename + "_ipasc.hdf5"
+            path = os.path.join(save_dir, full_filename)
+
+            show_sinogram(title, path)
+
+    plt.show()
