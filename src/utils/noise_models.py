8cf40708f8eec6b7200f9f75414bbde11ec35eba
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-05-30 10:34:07 +0200

    Scale noise
    Update the noise model such that the noise is scaled with respect to the
    data.

--- src/utils/noise_models.py
+++ src/utils/noise_models.py
@@ -1,28 +1,41 @@
+import os.path
+
 import pacfish as pf
 import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
+SNR = 2
 
-def add_noise(path_to_ipasc_hdf5: str, path_to_noise_mat:str):
-    pa_data = pf.load_data(path_to_ipasc_hdf5)
+
+def add_noise(path_to_ipasc_hdf5: str, path_to_noise_mat: str, snr: int=SNR):
+    """
+    Add noise to a simulated sinogram based on measured noise from the system.
+
+    :param str path_to_ipasc_hdf5: Path to the simulated sinogram without noise (.hdf5)
+    :param str path_to_noise_mat: Path to the noise file (.mat)
+    :param int snr: Signal to noise ratio used to scale the noise
+    """
+
+    sinogram = pf.load_data(path_to_ipasc_hdf5)
     noise_data = loadmat(path_to_noise_mat)
 
     normal_noise = noise_data["NoiseSinogram"]
-    mean_noise = np.swapaxes(np.mean(normal_noise, axis=2), 0, 1)
-    std_noise = np.swapaxes(np.std(normal_noise, axis=2), 0, 1)
+    mean_local_noise = np.swapaxes(np.mean(normal_noise, axis=2), 0, 1)
+    std_local_noise = np.swapaxes(np.std(normal_noise, axis=2), 0, 1)
 
-    ts = pa_data.binary_time_series_data.copy()
+    mean_data = np.mean(np.abs(sinogram.binary_time_series_data))
+    mean_noise = np.mean(np.abs(mean_local_noise))
 
-    # TODO SCALE THE SIMULATED TIME SERIES BY A FACTOR TO BE DETERMINED FROM A CALIBRATION PHANTOM
-    # TODO THIS SCALING WILL HAVE TO BE DETERMINED FOR EACH SIMULATION PIPELINE INDIVIDUALLY
-    ts = ts * 250000
-
-    print(np.shape(ts))
-
-    ts_noise = ts.copy() + np.random.normal(mean_noise, std_noise)
+    scaled_noise = np.random.normal(mean_local_noise, std_local_noise) / mean_noise * mean_data / SNR
 
     # TODO APPLY THE SIGMOID POST_PROCESSING EXACTLY HOW FRANCIS APPLIES IT TO THE DATA, TOO
 
-    pa_data.binary_time_series_data = ts_noise
-    pf.write_data(path_to_ipasc_hdf5.replace("_ipasc", "_noise_ipasc"), pa_data)
+    sinogram.binary_time_series_data += scaled_noise
+    pf.write_data(path_to_ipasc_hdf5.replace("_ipasc", "_noise_ipasc"), sinogram)
+
+
+if __name__ == "__main__":
+    data_path = "../../results/optical_attenuation_size_ipasc.hdf5"
+    noise_path = "../../resources/NoiseMeasurement.mat"
+    add_noise(os.path.abspath(data_path), os.path.abspath(noise_path))
