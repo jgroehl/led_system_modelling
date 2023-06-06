c604b741b5a5f3ebc0da2f9c174fe89851c2017d
Mirre van der Wal <mirrevanderwal@gmail.com>
2023-06-06 15:21:21 +0200

    Add normalization to noise model
    At first, we only normalized and saved the original sinograms, but not
    the ones created after applying the noise model. Now, we normalize all
    sinograms (with and without noise) and save these with '_noise' in the
    name, as .npy file.

--- src/utils/noise_models.py
+++ src/utils/noise_models.py
@@ -1,14 +1,15 @@
 import os.path
 
+import simpa as sp
 import pacfish as pf
 import numpy as np
 from scipy.io import loadmat
-import matplotlib.pyplot as plt
+from src.utils.normalize_sinogram import normalize_sinogram
 
 SNR = 2
 
 
-def add_noise(path_to_ipasc_hdf5: str, path_to_noise_mat: str, snr: int=SNR):
+def add_noise(path_to_ipasc_hdf5: str, path_to_noise_mat: str, snr: int = SNR, path_manager_path: str = ""):
     """
     Add noise to a simulated sinogram based on measured noise from the system.
 
@@ -27,15 +28,25 @@ def add_noise(path_to_ipasc_hdf5: str, path_to_noise_mat: str, snr: int=SNR):
     mean_data = np.mean(np.abs(sinogram.binary_time_series_data))
     mean_noise = np.mean(np.abs(mean_local_noise))
 
-    scaled_noise = np.random.normal(mean_local_noise, std_local_noise) / mean_noise * mean_data / SNR
+    scaled_noise = np.random.normal(mean_local_noise, std_local_noise) / mean_noise * mean_data / snr
 
     # TODO APPLY THE SIGMOID POST_PROCESSING EXACTLY HOW FRANCIS APPLIES IT TO THE DATA, TOO
 
     sinogram.binary_time_series_data += scaled_noise
-    pf.write_data(path_to_ipasc_hdf5.replace("_ipasc", "_noise_ipasc"), sinogram)
+    new_data_path = path_to_ipasc_hdf5.replace("_ipasc", "_noise_ipasc")
+    print("New data path: ", new_data_path)
+    pf.write_data(new_data_path, sinogram)
+
+    # Run normalization
+    path_manager = sp.PathManager(path_manager_path)
+    data_name = new_data_path.split("/")[-1].replace("_ipasc.hdf5", "")
+    normalized_sinogram = normalize_sinogram(new_data_path)
+    save_path = os.path.abspath(
+        os.path.join("../", path_manager.get_hdf5_file_save_path(), f"{data_name}_normalized.npy"))
+    np.save(save_path, normalized_sinogram)
 
 
 if __name__ == "__main__":
     data_path = "../../results/optical_attenuation_size_ipasc.hdf5"
     noise_path = "../../resources/NoiseMeasurement.mat"
-    add_noise(os.path.abspath(data_path), os.path.abspath(noise_path))
+    add_noise(os.path.abspath(data_path), os.path.abspath(noise_path), path_manager_path="../path_config.env")
